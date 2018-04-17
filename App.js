86eef1ec7a52a1acd38a4586ac2e402e1d2246e3
Jordan <guerreromets@gmail.com>
2018-04-16 22:20:42 -0400

    Insert image to recipe if one exists, styles list aswell

--- App.js
+++ App.js
@@ -1,45 +1,52 @@
-//
-// import { StyleSheet, Text, View, Text, Input, Linking, WebView } from 'react-native';
-//
-// export default class App extends Component<{}> {
-//
-//   render() {
-//
-//     return (
-//
-//       <View style={styles.MainContainer}>
-//
-//         <Text style={styles.TextStyle} onPress={ ()=> Linking.openURL('http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3') } >Click Here To Open Google.</Text>
-//
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//
-//   MainContainer: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center'
-//   },
-//
-//   TextStyle: {
-//
-//     color: '#E91E63',
-//     textDecorationLine: 'underline'
-//
-//   }
-// });
-
 import React from 'react';
-import { FlatList, ActivityIndicator, Text, View, Linking  } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View, Linking, StyleSheet, WebView, Image} from 'react-native';
 
-export default class FetchExample extends React.Component {
+class FlatListItem extends React.Component{
+	render() {
+	   return (
+		   <View style={{
+			   flex: 1,
+			   flexDirection:'column',
+		   }}>
+			   <View style={{
+					   flex: 1,
+					   flexDirection:'row',
+					   // backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen': 'tomato'
+					   backgroundColor: 'mediumseagreen'
+			   }}>
+				   <Image
+					   source={{uri: this.props.item.thumbnail}}
+					   style={{width: 100, height: 100, margin: 5}}
+				   >
+
+				   </Image>
+				   <View style={{
+						   flex: 1,
+						   flexDirection:'column',
+						   height: 100
+					   }}>
+						   <Text style={styles.flatListItem}>{this.props.item.title}</Text>
+						   <Text style={styles.TextStyle}>{this.props.item.ingredients}</Text>
+				   </View>
+			   </View>
+			   <View style={{
+				   height: 1,
+				   backgroundColor:'white'
+			   }}>
+
+			   </View>
+		 </View>
+	   );
+	}
+}
+export default class ChefMeUp extends React.Component {
 
   constructor(props){
     super(props);
-    this.state ={ isLoading: true}
+    this.state ={
+		isLoading: true,
+		showWebPart: false,
+		recipePage: ''}
   }
 
   componentDidMount(){
@@ -60,8 +67,6 @@ export default class FetchExample extends React.Component {
       });
   }
 
-
-
   render(){
 
     if(this.state.isLoading){
@@ -71,15 +76,57 @@ export default class FetchExample extends React.Component {
         </View>
       )
     }
-
+	else if (this.state.showWebPart) {
+		this.setState({showWebPart: false});
+		return(
+			<WebView
+				source={{uri:this.state.recipePage}}
+			/>
+		);
+	}
     return(
-      <View style={{flex: 1, paddingTop:20}}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => <Text onPress={ ()=>  Linking.openURL(item.href.replace(/\\/,"")) }>{item.title + "\n"}</Text>}
-          keyExtractor={(item, index) => index}
-        />
-      </View>
+      // <View style={{flex: 1, paddingTop:20}}>
+      //   <FlatList
+      //     data={this.state.dataSource}
+      //     renderItem={({item}) => <Text onPress={ e => this.setState({showWebPart:true, recipePage: Linking.openURL(item.href.replace(/\\/,""))})} >{item.title + "\n"}</Text>}
+      //     keyExtractor={(item, index) => index}
+      //   />
+      // </View>
+	  <View style={{flex: 1, marginTop: 22}}>
+            <FlatList
+                data={this.state.dataSource}
+                renderItem={({item, index})=>{
+                    //console.log(`Item = ${JSON.stringify(item)}, index = ${index}`);
+                    return (
+                    <FlatListItem item={item} index={index}>
+
+                    </FlatListItem>);
+                }}
+				keyExtractor={(item, index) => index}
+			>
+            </FlatList>
+	</View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+
+  MainContainer: {
+	flex: 1,
+	justifyContent: 'center',
+	alignItems: 'center'
+  },
+
+  TextStyle: {
+
+	color: '#E91E63',
+	textDecorationLine: 'underline'
+
+},
+  flatListItem: {
+        color: 'white',
+        padding: 10,
+        fontSize: 16,
+}
+});
