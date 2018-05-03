23f4b6af1729aff4b35aacb17fac4a1f7ab0a03b
Jordan <guerreromets@gmail.com>
2018-05-03 11:19:34 -0400

    Added drawer functionality and home page

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { StackNavigator } from 'react-navigation';
 import * as firebase from 'firebase';
 import { Input } from './components/Input';
 import { Button } from './components/Button';
+import AppNavigation from './AppNavigation';
+
+
 
 
 console.disableYellowBox = true;
@@ -51,252 +54,10 @@ class FlatListItem extends React.Component{
 	}
 }
 
-class LoginScreen extends React.Component{
-	state = {
-    email: '',
-    password: '',
-    authenticating: false,
-    user: null,
-    error: '',
-	}
-	onPressSignIn() {
-    this.setState({
-      authenticating: true,
-    });
-	
-	const { email, password } = this.state;
-	
-	firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => this.setState({
-        authenticating: false,
-        user,
-        error: '',
-      }))
-      .catch(() => {
-        // Login was not successful
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-          .then(user => this.setState({
-            authenticating: false,
-            user,
-            error: '',
-          }))
-          .catch(() => this.setState({
-            authenticating: false,
-            user: null,
-            error: 'Authentication Failure',
-          }))
-      })
-	}
-	
-	onPressLogOut() {
-    firebase.auth().signOut()
-      .then(() => {
-        this.setState({
-          email: '',
-          password: '',
-          authenticating: false,
-          user: null,
-        })
-      }, error => {
-        console.error('Sign Out Error', error);
-      });
-	}
-	  renderCurrentState() {
-    if (this.state.authenticating) {
-      return (
-        <View style={styles.form}>
-          <ActivityIndicator size='large' />
-        </View>
-      )
-    }
 
-    if (this.state.user !== null) {
-      return (
-		this.props.navigation.navigate('Search', {user: this.state.email})
-       // <View style={styles.form}>
-       //   <Text>Logged In</Text>
-       //   <Button onPress={() => this.onPressLogOut()}>Log Out</Button>
-       // </View>
-      )
-    }
-
-    return (
-      <View style={styles.form}>
-        <Input
-          placeholder='Enter your email...'
-          label='Email'
-          onChangeText={email => this.setState({ email })}
-          value={this.state.email}
-        />
-        <Input
-          placeholder='Enter your password...'
-          label='Password'
-          secureTextEntry
-          onChangeText={password => this.setState({ password })}
-          value={this.state.password}
-        />
-        <Button onPress={() => this.onPressSignIn()}>Log In</Button>
-        <Text>{this.state.error}</Text>
-      </View>
-    )
-
-  }
-
-  render() {
-    return (
-      <View style={styles.container1}>
-        {this.renderCurrentState()}
-      </View>
-    );
-  }
-}
-
- class RecipeScreen extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state ={
-		isLoading: true,
-		showWebPart: false,
-		recipePage: ''}
-  }
-
-  componentDidMount(){
-	console.log({food});
-    return fetch(recipeURL)
-      .then((response) => response.json())
-      .then((responseJson) => {
-
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson.results,
-        }, function(){
-
-        });
-
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-  }
-  // BackHandler.addEventListener('hardwareBackPress', function() {
-	//   console.log("TEST");
-  // });
-
-  render(){
-
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
-	else if (this.state.showWebPart) {
-		this.setState({showWebPart: false});
-		return(
-			<WebView
-				source={{uri:this.state.recipePage}}
-			/>
-		);
-	}
-    return(
-      // <View style={{flex: 1, paddingTop:20}}>
-      //   <FlatList
-      //     data={this.state.dataSource}
-      //     renderItem={({item}) => <Text onPress={ e => this.setState({showWebPart:true, recipePage: Linking.openURL(item.href.replace(/\\/,""))})} >{item.title + "\n"}</Text>}
-      //     keyExtractor={(item, index) => index}
-      //   />
-      // </View>
-	  <View style={{flex: 1, marginTop: 22}}>
-	  	<Text>{ingredients}</Text>
-            <FlatList
-                data={this.state.dataSource}
-                renderItem={({item, index})=>{
-                    //console.log(`Item = ${JSON.stringify(item)}, index = ${index}`);
-                    return (
-                    <FlatListItem item={item} index={index}>
-
-                    </FlatListItem>);
-                }}
-				keyExtractor={(item, index) => index}
-			>
-            </FlatList>
-	</View>
-    );
-  }
-}
-
-class SearchScreen extends React.Component{
-	constructor(props) {
-		super(props);
-		this.state = { text: '' };
-	}
-
-	// _handleTextChange = event => {
-  //   this.setState( {food: event.nativeEvent.food}).bind(this);
-  //   global.ingredients = this.state.food;
-  // }
-  
-  onPressLogOut() {
-    firebase.auth().signOut()
-      .then(() => {
-        this.setState({
-          email: '',
-          password: '',
-          authenticating: false,
-          user: null,
-        })
-		this.props.navigation.navigate('Login')
-      }, error => {
-        console.error('Sign Out Error', error);
-      });
-	}
-
-  sendData = (data)=>{
-	  // this._handleTextChange.bind(this);
-	  this.setState({text:data}, function(){
-		  foodArray = data.split(',');
-		  var i;
-		  	for(i = 0; i < foodArray.length; i++){
-				recipeURL+= (foodArray[i]+",");
-			}
-          global.food = this.state.text;
-    	  this.props.navigation.navigate('Recipe', {food: this.state.text});
-	  });
-  }
-
-	render(){
-		return (
-			<View style = {styles.container}>
-				<Text style={styles.otherText}>Write ingredients, separate each with comma, no spaces:</Text>
-				<TextInput style={styles.nameInput} onSubmitEditing={(event) => this.sendData(event.nativeEvent.text)}/>
-				<Button onPress={() => this.onPressLogOut()}>Log Out</Button>
-		   </View>
-		 );
-	}
-
-}
-
-const RootStack = StackNavigator(
-  {
-    Recipe: {
-      screen: RecipeScreen,
-    },
-    Search: {
-      screen: SearchScreen,
-    },
-	Login: {
-		screen: LoginScreen,
-	},
-},
-  {
-    initialRouteName: 'Login',
-  }
-);
 
 export default class App extends React.Component {
-  
+
   componentWillMount(){
 	var config = {
 		apiKey: "AIzaSyD5e9uuw6-6UqxYKS3Gf6IMxjkYQsGim9Y",
@@ -310,7 +71,7 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <RootStack />;
+    return <AppNavigation />;
   }
 }
 
